aa308d6743457a5af81a84730a61eb8e2cc87ae2
unknown <mf41444@INN14U-DW1496.CORP.CYIENT.COM>
2017-06-22 10:43:01 +0530

    first commit --adapter design pattern

---
+++ src/adapterdesignpattern/EnemyAttacker.java
@@ -0,0 +1,10 @@
+package adapterdesignpattern;
+
+public interface EnemyAttacker {
+	
+	public void fireWeapon();
+	
+	public void driveForward();
+	
+	public void assignDriver(String driverName);
+}
---
+++ src/adapterdesignpattern/TestEnemyAttackers.java
@@ -0,0 +1,34 @@
+package adapterdesignpattern;
+
+public class TestEnemyAttackers {
+
+	public static void main(String[] args) {
+		EnemyTank rx7Tank=new EnemyTank();
+		
+		EnemyRobot fredTheRobot=new EnemyRobot();
+		
+		EnemyRobotAdapter robotAdapter=new EnemyRobotAdapter(fredTheRobot);
+		
+		System.out.println("The Robot");
+		
+		fredTheRobot.reactToHuman("Faisal");
+		fredTheRobot.walkForward();
+		fredTheRobot.smashWithHands();
+		
+		System.out.println("The Enemy Tank");
+		
+		rx7Tank.assignDriver("Frank");
+		rx7Tank.driveForward();
+		rx7Tank.fireWeapon();
+		
+		System.out.println("The Robot with Adapter");
+		
+		robotAdapter.assignDriver("Mark");
+		robotAdapter.driveForward();
+		robotAdapter.fireWeapon();
+		
+		
+
+	}
+
+}
